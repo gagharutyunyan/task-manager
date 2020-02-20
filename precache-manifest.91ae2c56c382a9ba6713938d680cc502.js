8d36a1f5a78463863b93f880127ba8145227882e
gagharutyunyan <gagharut@gmail.com>
2020-02-20 14:59:18 +0300

    Updates

---
+++ precache-manifest.91ae2c56c382a9ba6713938d680cc502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b05414f9d39f30fb412c58298edcdf",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "ee5daed310a7552f505c",
+    "url": "/task-manager/static/css/main.6fc28d5f.chunk.css"
+  },
+  {
+    "revision": "5adf45ea1d858d547530",
+    "url": "/task-manager/static/js/2.79837108.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/task-manager/static/js/2.79837108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5daed310a7552f505c",
+    "url": "/task-manager/static/js/main.040ef071.chunk.js"
+  },
+  {
+    "revision": "3b8a76c99c24c2021529",
+    "url": "/task-manager/static/js/runtime-main.94bac786.js"
+  }
+]);
